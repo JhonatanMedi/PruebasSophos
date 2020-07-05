5102f461b024090532731ea00d856ae745fa0fb9
Your NamJhonatan <blanco.jhonatan@hotmail.com>
2020-07-05 12:28:37 -0500

    se configura las dependencia

---
+++ src/main/java/SophosPruebas/SophosPruebas/App.java
@@ -0,0 +1,13 @@
+package SophosPruebas.SophosPruebas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
